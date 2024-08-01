59dde6dc06d2dbf4c15bc168433680dc4e75fc96
cysabi <47790183+cysabi@users.noreply.github.com>
2024-08-01 03:53:20 -0400

    fix trailling slash

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import solidPlugin from "vite-plugin-solid";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@tailwindcss/vite";
 import devtools from "solid-devtools/vite";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
-import vikeSolid from "vike-solid/vite";
+import solid from "vike-solid/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), vike({ prerender: true }), vikeSolid(), devtools()],
+  plugins: [
+    tailwind(),
+    vike({ prerender: true, trailingSlash: false }),
+    solid(),
+    devtools(),
+  ],
 });
